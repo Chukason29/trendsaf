6485db32cb78f2e3eb0c151b611ab03be0d3e814
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-10-16 00:07:37 +0100

    confirmation

--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -219,12 +219,7 @@ def confirmation():
             #TODO get the jwt token from the header and extract
             id = decode_id(get_jwt_identity())
 
-
             session_id = decode_id(session.get("user_uuid"))
-            return jsonify({
-                "session_id": session_id,
-                "id": id
-            })
             #making sure that jwt identity and session identity is same
             if not session_id == id:
                 abort(401)
