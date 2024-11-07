580f9d47d730ad3e4fb7187d9d675758dbc5b46b
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-11-07 06:53:26 +0100

    admin routes changes

--- app/blueprints/general_routes/routes.py
+++ app/blueprints/general_routes/routes.py
@@ -1,25 +1,41 @@
 from flask import Blueprint, request, jsonify, abort
 from sqlalchemy import Column, Integer, String, and_, desc, asc
-from ...models import Users, Crops, Countries, Regions
+from ...models import Users, Crops, Countries, Regions, CropCategories
 from ...config import Config
 import html
 import json
 
 general_bp = Blueprint('general_routes', __name__)
-@general_bp.route('/get_crops', methods = ['POST', 'GET'])
+@general_bp.route('/crops', methods = ['POST', 'GET'])
 def get_crops():
     crops = Crops.query.order_by(asc(Crops.crop_name)).all()
     all_crops = [{"id": crop.crop_id, "name": crop.crop_name} for crop in crops]
     return jsonify(all_crops)
 
-@general_bp.route('/get_countries', methods = ['POST', 'GET'])
+@general_bp.route('/cropcategories', methods = ['POST', 'GET'])
+def get_crops():
+    crop = request.get_json()
+    #TODO check is certain params are missing
+    if "crop_id" not in crop:
+        abort(422)
+    crop_id = crop['crop_id']
+    cropCategories = CropCategories.query.filter_by(crop_id=crop_id).all()
+    all_crops = [
+        {
+            "id": cropCategory.crop_categories_id, 
+            "variety": cropCategory.crop_variety, 
+            "code":cropCategory.crop_code
+        } for cropCategory in cropCategories]
+    return jsonify(all_crops)
+
+@general_bp.route('/countries', methods = ['POST', 'GET'])
 def get_countries():
     countries = Countries.query.all()
     all_countries = [{"id": country.country_id, "name": country.country_name, "code": country.country_code } for country in countries]
     return jsonify(all_countries)
 
 
-@general_bp.route('/get_regions', methods = ['POST', 'GET'])
+@general_bp.route('/regions', methods = ['POST', 'GET'])
 def get_regions():
     #TODO get the country of the region needed
     country = request.get_json()
