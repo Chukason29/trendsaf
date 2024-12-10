bffa402ae1733e2292a851487eab3386c50ff8ac
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-12-10 15:22:19 +0100

    crop charts

--- app/blueprints/user/routes.py
+++ app/blueprints/user/routes.py
@@ -19,7 +19,7 @@ import json
 import pendulum
 
 user_bp = Blueprint('user', __name__)
-@user_bp.route('/crops/prices',  methods=['POST', 'GET'])
+@user_bp.route('/crops/prices',  methods=['POST'])
 @jwt_required()
 def crop_prices():
     try:
@@ -77,6 +77,76 @@ def crop_prices():
             "min_price" : row.min_price/100,
             "price_change" : row.max_price/row.min_price,
             "average_price": float(row.average_price)/100 if row.average_price is not None else None
+        }
+        for row in result
+        ]
+    
+        return jsonify(result_json)
+        
+        
+        
+    except:
+        db.session.rollback()
+        raise
+
+
+@user_bp.route('/crops/chart',  methods=['POST'])
+@jwt_required()
+def crop_chart():
+    try:
+        #TODOGetting the user's id
+        id = uuid.UUID(decode_id(get_jwt_identity()))
+
+        #Retrieve authorization token
+        auth_token = request.headers.get("Authorization").split(" ")[1]
+        user_data = decode_token(auth_token, allow_expired=False)
+        
+        user_query = Users.query.filter_by(user_uuid = id).first()
+
+        #Getting request body
+        data = request.get_json()
+        if not is_json(data):
+            abort(415)
+            
+        if 'crop_id' not in data or 'crop_variety_id' not in data or 'country_id' not in data or 'duration' not in data:
+            abort(422)
+            
+        #TODO get the values of crop_variety_id and country_id
+        crop_id = data['crop_id']
+        crop_variety_id = data['crop_variety_id']
+        country_id = data['country_id']
+        duration = data['duration']
+        
+        #.filter(Product.created_at.between(current_duration, now)) \
+        #TODO get today's date using python
+        now = pendulum.now()
+        
+        if duration == "week":   
+            current_duration = now.start_of("week").subtract(days=1)
+            previous_duration = current_duration.subtract(weeks=1)
+            
+        elif duration == "month":   
+            current_duration = now.start_of("month")
+            previous_duration = current_duration.subtract(months=1)
+                
+        result = db.session.query(
+        CropVariety.crop_variety_id.label('variety_id'),
+            CropVariety.crop_variety_name.label('variety_name'),
+            Product.price.label('price'),
+            Product.created_at.label('date'),
+
+        ).join(Product, CropVariety.crop_variety_id == Product.crop_variety_id) \
+        .filter(Product.crop_id == crop_id) \
+        .filter(Product.country_id == country_id) \
+        .filter(Product.crop_variety_id == crop_variety_id) \
+        .filter(Product.created_at >= current_duration) \
+        .group_by(CropVariety.crop_variety_id) \
+        .all()
+
+        result_json = [
+        {
+            "x": row.price,
+            "y": row.date,
         }
         for row in result
         ]
