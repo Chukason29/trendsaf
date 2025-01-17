0247568f2e9fc8d28dfc784a1243747a9cbed612
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2025-01-17 01:43:08 +0100

    routing

--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -41,14 +41,14 @@ def login():
         if not user:
             return jsonify({
                 "status" : False,
-                "message" : "wrong email or passwordddddddd",
+                "message" : "wrong email or passwordcd",
                 "base_url" : Config.BASE_URL
             })
             #checked if there is a password match
         if not (password and bcrypt.check_password_hash(user.password, password)):
             return jsonify({
                 "status" : False,
-                "message" : "wrong email or passworddddddddd",
+                "message" : "wrong email or password",
                 "base_url" : Config.BASE_URL
             })
         #TODO collected the uuid of the user encode it and use as the identity of the user in the JWT
