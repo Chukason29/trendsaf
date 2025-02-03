a499c7f4badd6fee89f352b72149e5e0596b8cd8
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2025-02-03 22:47:17 +0100

    admin login

--- app/blueprints/admin/routes.py
+++ app/blueprints/admin/routes.py
@@ -150,7 +150,7 @@ def reset_password(token):
             abort(415)
         
         #check if all required parameters are contained in the json body
-        if 'token' not in data or 'initial_password' not in data or 'new_password' not in data or 'confirm_password' not in data:
+        if 'initial_password' not in data or 'new_password' not in data or 'confirm_password' not in data:
             abort(422)
         serializer = URLSafeTimedSerializer(Config.SECRET_KEY)
         admin_uuid = serializer.loads(token, salt=Config.RESET_PASSWORD_SALT, max_age=3600)# 15 Minutes
