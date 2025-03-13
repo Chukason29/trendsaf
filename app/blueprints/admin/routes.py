d7054fc391d7350f56b10e319328375169863e5b
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2025-03-13 19:21:11 +0100

    resolve CORS

--- app/blueprints/admin/routes.py
+++ app/blueprints/admin/routes.py
@@ -619,8 +619,10 @@ def import_data():
         raise
     
 
-@app.after_request
+@admin_bp.after_request
 def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = request.headers.get("Origin", "*")
     response.headers["Access-Control-Allow-Credentials"] = "true"
-    response.headers['Access-Control-Allow-Origin'] = request.headers.get('Origin')
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization, X-CSRF-TOKEN"
     return response
